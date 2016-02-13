75a2f9bef50335d6e3119ee2ff4889bec3075527
Tommy Brander <tommy@brander.nu>
2016-02-13 17:06:31 +0100

    Menu changes

---
+++ resources/js/site.js
@@ -0,0 +1,14 @@
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 100) {
+    $('#pageheader').css('position', 'fixed');
+    $('#pageheader').css('height', '50px');
+    $('#pagetitle').css('line-height', '50px');
+    $('#pagenav').css('line-height', '50px');
+  } else {
+    $('#pageheader').css('position', 'static');
+    $('#pageheader').css('height', '100px');
+    $('#pagetitle').css('line-height', '100px');
+    $('#pagenav').css('line-height', '100px');
+  }
+});
